64add7275199af3652814454be49c93f14c66639
saridiramesh <152684848+saridiramesh@users.noreply.github.com>
2024-05-23 20:15:02 +0530

    Create jenkins-ubuntu.sh

---
+++ jenkins-ubuntu.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install fontconfig openjdk-11-jre
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \ 
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get install jenkins
+systemctl start jenkins.service
+systemctl status jenkins.service
