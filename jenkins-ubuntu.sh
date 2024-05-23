c21d461707942d44f1b00434de49936390dc3476
saridiramesh <152684848+saridiramesh@users.noreply.github.com>
2024-05-23 20:19:43 +0530

    Update jenkins-ubuntu.sh

--- jenkins-ubuntu.sh
+++ jenkins-ubuntu.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install fontconfig openjdk-11-jre
+sudo apt-get install fontconfig openjdk-11-jre -y
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \ 
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
