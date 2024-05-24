575311d47f3dcc71946c867a5aeb6e46c829c03c
saridiramesh <152684848+saridiramesh@users.noreply.github.com>
2024-05-24 11:07:28 +0530

    Update jenkins-ubuntu.sh

--- jenkins-ubuntu.sh
+++ jenkins-ubuntu.sh
@@ -1,11 +1,11 @@
 sudo apt-get update
 apt install maven -y
 sudo apt-get install fontconfig openjdk-11-jre -y
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \ 
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get install jenkins
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get install jenkins -y
 systemctl start jenkins.service
 systemctl status jenkins.service
